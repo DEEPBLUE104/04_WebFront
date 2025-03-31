c2e36d3fa1ddd6da2ebc4e891577bb7764ea14d5
DEEPBLUE104 <dlara7173@gmail.com>
2025-03-31 15:34:49 +0900

    요소활용방법

--- 3_Javascript/js/05_반복문, 분기문.js
+++ 3_Javascript/js/05_반복문, 분기문.js
@@ -45,6 +45,63 @@ function check1(){
 
     alert(`총가격 : ${sum}`)
 
+}
 
+//Up & Down Game
+function startGame() {
+
+  // 맞춰야 하는 난수(1~200) 발생시켜 저장
+  const answer = Math.floor(Math.random()*200) + 1;
+
+  // 정답 시도 횟수를 세기 위한 변수 선언
+  let count = 0;
+
+  // prompt에 출력할 문자열 
+  let str = '1부터 200사이 숫자 입력'
+
+  while(true) { // 무한 반복
+
+    let input = prompt(str);
+
+    if ( input === null ){ //취소
+      alert("게임포기");
+      break;
+    }
+
+    // 숫자 입력 후 확인 클릭시
+    const value = Number(input);  //입력 받은 값 숫자로 변환
+
+    // 숫자가 아닌 문자열같이 잘못 입력한 경우
+    // NaN (Not a Number : 숫자가 아니다.)
+    // isNaN(값) : 값이 NaN 이면 true 출력
+
+    if(isNaN(value)) { //숫자가 아닌 값을 입력한 경우
+      alert("숫자만 입력해주세요");
+      continue;
+    }
+
+    if(value <1 || value > 200) { // 범위초과
+       alert("1 ~ 200 사이 값만 작성해주세요")
+       continue;
+    }
+    
+    // 정답을 맞추기 위한 시도를 했기 때문에 count를 1 증가 시켜야 한다.
+    count++;
+
+    // 정답인 경우
+
+    if(value === answer) {
+      alert(`정답! (${answer}) / 시도 횟수: ${count}`);
+      break; //정답을 맞췄으니 반복 멈춤
+    }
+
+    // 정답이 아닌 경우
+    if( value < answer) {  //우리가 입력한 값이 작은 경우
+      str = `${value} [UP] / 시도횟수 ${count}` 
+    } else { // 큰 경우
+      str = `${value} [DOWN] / 시도횟수 ${count}` 
+    }
+
+  }
 
 }
